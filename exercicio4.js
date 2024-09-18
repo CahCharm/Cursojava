71d32ea2069e802e3b71ddce6a0ea3adbcca8691
cahcharm <126590861+CahCharm@users.noreply.github.com>
2024-09-18 08:50:34 -0300

    .

--- exercicio4.js
+++ exercicio4.js
@@ -1,5 +1,9 @@
-// Declaração de variáveis let raio = 7; // Raio do círculo const pi 3.14159; // Valor de pi
+// Declaração de variáveis 
+let raio = 7; // Raio do círculo 
+const pi= 3.14159; // Valor de pi
 
-// Cálculo do perímetro let perimetro 2 pi raio;
+// Cálculo do perímetro 
+let perimetro = 2 * pi * raio;
 
-// Exibe o resultado no console console.log("O perímetro do círculo é: " + perimetro);
+// Exibe o resultado no console 
+console.log("O perímetro do círculo é: " + perimetro);
